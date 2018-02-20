50fd610f63049a2e66de9eb349c21e00a1f055a7
Johanna de Vos <johannadevos@gmail.com>
2018-02-20 11:06:40 +0100

    Added a regex to remove URLs to KaggleWord2VecUtility.py, and silenced Beautiful Soup warnings (having checked their relevance first).

--- src/KaggleWord2VecUtility.py
+++ src/KaggleWord2VecUtility.py
@@ -2,6 +2,7 @@
 
 import re
 import nltk
+import warnings
 
 import pandas as pd
 import numpy as np
@@ -9,6 +10,8 @@ import numpy as np
 from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
 
+# Turn off warnings about Beautiful Soup (Johanna has checked all of them manually)
+warnings.filterwarnings("ignore", category=UserWarning, module='bs4')
 
 class KaggleWord2VecUtility(object):
     """KaggleWord2VecUtility is a utility class for processing raw HTML text into segments for further learning"""
@@ -21,29 +24,31 @@ class KaggleWord2VecUtility(object):
         # 1. Remove HTML
         review_text = BeautifulSoup(review, 'html.parser').get_text()
         #
-        # 2. Remove non-letters
+        # 2. Remove URLs/links by means of a regular expression.
+        review_text = re.sub("(www.|http[s]?:\/)(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", "", review_text)
+        #
+        # 3. Remove non-letters
         review_text = re.sub("[^a-zA-Z]"," ", review_text)
         #
-        # 3. Convert words to lower case and split them
+        # 4. Convert words to lower case and split them
         words = review_text.lower().split()
         #
-        # 4. Optionally remove stop words (false by default)
+        # 5. Optionally remove stop words (false by default)
         if remove_stopwords:
             stops = set(stopwords.words("english"))
             words = [w for w in words if not w in stops]
         #
-        # 5. Return a list of words
+        # 6. Return a list of words
         return(words)
 
     # Define a function to split a review into parsed sentences
     @staticmethod
-    def review_to_sentences(review : bytes, tokenizer,
-        remove_stopwords : bool = False ):
+    def review_to_sentences(review : bytes, tokenizer, remove_stopwords : bool = False ):
         # Function to split a review into parsed sentences. Returns a
         # list of sentences, where each sentence is a list of words
         #
         # 1. Use the NLTK tokenizer to split the paragraph into sentences
-        raw_sentences = tokenizer.tokenize(review.strip())
+        raw_sentences = tokenizer.tokenize(review.strip()) # strip removes leading or trailing whitespace
         #
         # 2. Loop over each sentence
         sentences = []
@@ -51,8 +56,7 @@ class KaggleWord2VecUtility(object):
             # If a sentence is empty, skip it
             if len(raw_sentence) > 0:
                 # Otherwise, call review_to_wordlist to get a list of words
-                sentences.append(KaggleWord2VecUtility.review_to_wordlist(raw_sentence, \
-                  remove_stopwords))
+                sentences.append(KaggleWord2VecUtility.review_to_wordlist(raw_sentence, remove_stopwords))
         #
         # Return the list of sentences (each sentence is a list of words,
         # so this returns a list of lists
