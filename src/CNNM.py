5a3258dde4e62841f4128c718176f61a9c993464
Andre <andre@ubuntu.ubuntu-domain>
2018-04-04 14:58:38 +0200

    latest version4 april

--- src/CNNM.py
+++ src/CNNM.py
@@ -3,6 +3,9 @@
 """ can be used on sequences, such as the one-dimensional sequence of words in a movie review. The same properties that make the CNN model attractive for learning to recognize objects in images can help to learn structure in paragraphs of words, namely the techniques invariance to the specific position of features."""
 
 # CNN for the IMDB problem
+import os
+import sys
+from pathlib import Path
 import numpy as np
 from keras.datasets import imdb
 from keras.models import Sequential
@@ -19,6 +22,40 @@ from keras import optimizers
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping
 import matplotlib.pyplot as plt
+import pandas as pd
+
+def _get_current_file_dir() -> Path:
+    """Returns the directory of the script."""
+    try:
+        return Path(os.path.realpath(__file__)).parent
+    except(NameError):
+        return Path('')
+
+
+# Project root directory, i.e. the github repo directory.
+_PROJECT_ROOT = _get_current_file_dir() / '..'
+
+# Where to find the data files.
+_DATA_FILE = {
+    'labeled':
+        _PROJECT_ROOT / 'data' / 'labeledTrainData.tsv',
+    'unlabeled':
+        _PROJECT_ROOT / 'data' / 'unlabeledTrainData.tsv',
+    'test':
+        _PROJECT_ROOT / 'data' / 'testData.tsv',
+    'result':
+        _PROJECT_ROOT / 'results' / 'PredictionCNNM.csv'
+}
+
+
+def read_data(keyword: str) -> pd.DataFrame:
+    """
+    Reads data from the tsv file corresponding to ``_DATA_FILE[keyword]``
+    and returns it as a :py:class:`panda.DataFrame`.
+    """
+    return pd.read_csv(_DATA_FILE[keyword], header=0, delimiter='\t',
+                       quoting=3)
+
 
 ##definition for plotting the model
 def plot_model_history(model_history):
@@ -41,12 +78,18 @@ def plot_model_history(model_history):
     axs[1].legend(['train_set', 'validation_set'], loc='best')
     plt.show()
 
+
+X_train_data = read_data('labeled')
+X_test_data = read_data('test')
+
+
+
 # fix random seed for reproducibility
 seed = 7
 np.random.seed(seed)
 
 # load the dataset but only keep the top n words, zero the rest
-top_words = 25000
+top_words = 5000
 (X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=top_words)
 # pad dataset to a maximum review length in words
 max_words = 500
@@ -69,16 +112,26 @@ model.add(Dense(1, activation='sigmoid', use_bias= True))
 model.compile(loss='binary_crossentropy', optimizer=Adam(lr=0.0021), metrics=['accuracy']) #TRY OPTIMIZERS
 print(model.summary())
 
-#
+
 #Stop training when a monitored quantity has stopped improving.
 #Patience how much should it be tolerable.
 earlystop = EarlyStopping(patience = 1)
 callbacks_list = [earlystop]
 # Fit the model ( a number of epochs means how many times you go through your training set.)
-model_info = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=4, batch_size=128, verbose=2, callbacks=callbacks_list)
+model_info = model.fit(X_train, y_train, epochs=4, batch_size=128, verbose=2, callbacks=callbacks_list)
+#calculate predictions
+predictions = model.predict(X_test)
+# round predictions
+result = [round(x[0]) for x in predictions]
+
+pd.DataFrame(data={"id": X_test_data["id"], "sentiment": result}) \
+	.to_csv(_DATA_FILE['result'], index=False, quoting=3)
+#preds = pd.DataFrame(rounded, columns=['predictions']).to_csv('predictionCNN.csv')
+#np.savetxt('~/BagofWords/results/CNNMpreds.csv', rounded, delimiter=',')
 # plot model history
 plot_model_history(model_info)
 # Final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
+
 
